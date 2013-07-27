8623325904afb2225b7cac533883e3f02f76b908
Jeff Bisbee <jeff.bisbee@smartbear.com>
2013-07-27 00:04:14 -0400

    its always the alternate bind for OSX.  I was on my Ubuntu vagrant and didnt realize it

--- pythonstartup.py
+++ pythonstartup.py
@@ -13,8 +13,7 @@ except ImportError as exception:
     print('Shell Enhancement module problem: {1}').format(exception)
 else:
     # Enable Tab Completion
-    if sys.platform == 'darwin' and int(platform.release().split('.')[0]) < 12:
-        # differnt bind for Lion and previous, seems to be fixed in Mountain Lion
+    if sys.platform == 'darwin':     # different bind for OSX
         readline.parse_and_bind("bind ^I rl_complete")
     else:
         readline.parse_and_bind("tab: complete")
